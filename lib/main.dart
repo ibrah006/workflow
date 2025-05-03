cfe3e065471f3b685a975b60796dbaf52aa8cd52
ibrahimmn <ibrahimmn006@gmail.com>
2025-05-03 13:28:54 +0300

    latest changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,13 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:workflow/login_screen.dart';
-=======
 import 'package:provider/provider.dart';
 import 'package:workflow/core/config/app_routes.dart';
 import 'package:workflow/core/providers/dashboard_details_provider.dart';
 import 'package:workflow/core/services/login_service.dart';
->>>>>>> project_restrucutre
 
 void main(List<String> args) {
   runApp(WorkflowApp());
 }
 
-<<<<<<< HEAD
-class WorkflowApp extends StatelessWidget {
-  const WorkflowApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        // primaryColor: Color(0xFF3776e8),
-        // color
-        colorScheme: ColorScheme(
-          brightness: Brightness.light, // You can choose light or dark here
-          primary: Colors.indigo, // Main button color
-          onPrimary: Colors
-              .white, // Text/icons on primary color (White text for buttons)
-          secondary: Color(0xFF00bcd4), // Secondary color, you can adjust this
-          onSecondary: Colors.black, // Text/icons on secondary color
-          error: Colors.red, // Error color, can use red or any color you prefer
-          onError:
-              Colors.white, // Text/icons on error color (White text for error)
-          surface: Colors.white, // Surface background color (e.g., cards)
-          onSurface:
-              Colors.black, // Text/icons on surface background (Black text)
-          background: Colors.grey[50]!, // Background color (light grey)
-          onBackground:
-              Colors.black, // Text/icons on background color (Black text)
-        ),
-        textTheme: TextTheme(
-          headlineLarge: TextStyle(
-            fontSize: 37,
-            fontWeight: FontWeight.bold,
-          ),
-          headlineMedium: TextStyle(
-            fontSize: 30, // ~80% of large
-            fontWeight: FontWeight.w600, // Slightly lighter
-          ),
-          headlineSmall: TextStyle(
-            fontSize: 24, // ~65% of large
-            fontWeight: FontWeight.w500,
-          ),
-          titleLarge: TextStyle(
-            fontSize: 24, // ~25% larger than medium
-            fontWeight: FontWeight.w700, // Slightly bolder for emphasis
-            color: Color(0xFF455763),
-          ),
-          titleMedium: TextStyle(
-            fontSize: 19,
-            fontWeight: FontWeight.w600,
-            color: Color(0xFF455763),
-          ),
-          titleSmall: TextStyle(
-            fontSize: 16, // ~15% smaller than medium
-            fontWeight: FontWeight.w500,
-            color: Color(0xFF455763),
-          ),
-        ),
-        textButtonTheme: TextButtonThemeData(
-          style: ButtonStyle(
-            textStyle:
-                WidgetStatePropertyAll(Theme.of(context).textTheme.titleMedium),
-          ),
-        ),
-        filledButtonTheme: FilledButtonThemeData(
-          style: ButtonStyle(
-            textStyle:
-                WidgetStatePropertyAll(Theme.of(context).textTheme.titleMedium),
-          ),
-        ),
-      ),
-      home: LoginScreen(),
-    );
-  }
-=======
 class WorkflowApp extends StatefulWidget {
   const WorkflowApp({super.key});
 
@@ -209,5 +132,4 @@ class _WorkflowAppState extends State<WorkflowApp> {
       });
     });
   }
->>>>>>> project_restrucutre
 }
