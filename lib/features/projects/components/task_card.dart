b7e0b49ffe4bca6a9ad08df4e77ef26864e426a8
ibrahimmn <ibrahimmn006@gmail.com>
2025-05-07 19:32:06 +0300

    separated task card (view from project details) toa different file

---
+++ lib/features/projects/components/task_card.dart
@@ -0,0 +1,80 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+
+class TaskCard extends StatelessWidget {
+  const TaskCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        border:
+            Border.all(width: 1.75, color: Color.fromARGB(255, 233, 237, 238)),
+      ),
+      child: DefaultTextStyle(
+        style: textTheme.bodyLarge!.copyWith(color: Colors.blueGrey.shade900),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Task title
+            Text("Design Mockups",
+                style: textTheme.titleMedium!.copyWith(
+                    color: Colors.black, fontWeight: FontWeight.w500)),
+            SizedBox(height: 5),
+            // Assignees
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 12,
+                  child: Icon(
+                    EvaIcons.person,
+                    size: 20,
+                  ),
+                ),
+                Expanded(child: SizedBox()),
+                Icon(EvaIcons.archiveOutline, size: 19),
+                SizedBox(width: 5),
+                Text("In Progress"),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                // Due date
+                Icon(EvaIcons.calendarOutline, size: 19),
+                SizedBox(width: 5),
+                Text("Feb 23"),
+                // TODO: Implement the backend for this
+                // TODO: Total time clocked in by users for this task.
+                Expanded(child: SizedBox()),
+                Text("12h")
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Icon(
+                  EvaIcons.alertTriangle,
+                  color: Color(0xFFfdc800),
+                  size: 19,
+                ),
+                SizedBox(width: 5),
+                Text("Material Wastage",
+                    style: textTheme.titleSmall!
+                        .copyWith(color: Color(0xFFf7303a))),
+                Expanded(child: SizedBox()),
+                Icon(EvaIcons.chevronRight,
+                    size: 28, color: Colors.blueGrey.shade900)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/projects/sections/project_tasks_section.dart
+++ lib/features/projects/sections/project_tasks_section.dart
@@ -1,6 +1,6 @@
-import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:workflow/data/models/project.dart';
+import 'package:workflow/features/projects/components/task_card.dart';
 
 class ProjectTasksSection extends StatelessWidget {
   const ProjectTasksSection({super.key, required this.project});
@@ -42,77 +42,7 @@ class ProjectTasksSection extends StatelessWidget {
                 mainAxisSpacing: 10.0,
                 childAspectRatio: screenWidth > 530 ? 1.05 : 2.25,
                 children: List.generate(6, (index) {
-                  return Container(
-                    padding: EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(
-                          width: 1.75,
-                          color: Color.fromARGB(255, 233, 237, 238)),
-                    ),
-                    child: DefaultTextStyle(
-                      style: textTheme.bodyLarge!
-                          .copyWith(color: Colors.blueGrey.shade900),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          // Task title
-                          Text("Design Mockups",
-                              style: textTheme.titleMedium!.copyWith(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w500)),
-                          SizedBox(height: 5),
-                          // Assignees
-                          Row(
-                            children: [
-                              CircleAvatar(
-                                radius: 12,
-                                child: Icon(
-                                  EvaIcons.person,
-                                  size: 20,
-                                ),
-                              ),
-                              Expanded(child: SizedBox()),
-                              Icon(EvaIcons.archiveOutline, size: 19),
-                              SizedBox(width: 5),
-                              Text("In Progress"),
-                            ],
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              // Due date
-                              Icon(EvaIcons.calendarOutline, size: 19),
-                              SizedBox(width: 5),
-                              Text("Feb 23"),
-                              // TODO: Implement the backend for this
-                              // TODO: Total time clocked in by users for this task.
-                              Expanded(child: SizedBox()),
-                              Text("12h")
-                            ],
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              Icon(
-                                EvaIcons.alertTriangle,
-                                color: Color(0xFFfdc800),
-                                size: 19,
-                              ),
-                              SizedBox(width: 5),
-                              Text("Material Wastage",
-                                  style: textTheme.titleSmall!
-                                      .copyWith(color: Color(0xFFf7303a))),
-                              Expanded(child: SizedBox()),
-                              Icon(EvaIcons.chevronRight,
-                                  size: 28, color: Colors.blueGrey.shade900)
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  );
+                  return TaskCard();
                 }),
               ),
             ),
